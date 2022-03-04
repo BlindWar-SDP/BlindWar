1d837b85808c46fa9b5882515787bad37e98429e
Marty <mathias.marty@epfl.ch>
2022-03-04 16:48:10 +0100

    Add the base logic tase logic to play a music

--- app/src/main/java/ch/epfl/sdp/blindwar/GreetingActivity.kt
+++ app/src/main/java/ch/epfl/sdp/blindwar/GreetingActivity.kt
@@ -3,11 +3,15 @@ package ch.epfl.sdp.blindwar
 import android.os.Bundle
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import ch.epfl.sdp.blindwar.game.GameTutorial
 
 class GreetingActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_greeting)
+
+        val game = GameTutorial(assets)
+        game.nextRound()
 
         // Get the Intent that started this activity and extract the string
         val message = intent.getStringExtra(EXTRA_MESSAGE)
--- app/src/main/java/ch/epfl/sdp/blindwar/game/GameTutorial.kt
+++ app/src/main/java/ch/epfl/sdp/blindwar/game/GameTutorial.kt
@@ -1,12 +1,43 @@
 package ch.epfl.sdp.blindwar.game
 
-class GameTutorial: Game() {
+import android.content.res.AssetFileDescriptor
+import android.content.res.AssetManager
+import android.media.MediaMetadataRetriever
+import android.media.MediaPlayer
+import android.util.Log
+
+
+/**
+ * Game logic for the tutorial
+ *
+ * @constructor
+ * Create an instance of the tutorial's game logic
+ *
+ * @param assetManager AssetManager instance to get the mp3 files
+ */
+class GameTutorial(assetManager: AssetManager): Game() {
+    private val assetManager = assetManager
+    private val mediaMetadataRetriever: MediaMetadataRetriever = MediaMetadataRetriever()
+
     override fun nextRound() {
-        TODO("Not yet implemented")
+        val afd: AssetFileDescriptor = assetManager.openFd("Gorillaz-Feel Good Inc.mp3")
+        assetManager.list("")?.filter { it.endsWith(".mp3") } ?.forEach { Log.d("MUSIC", it.toString()) }
+
+        val player = MediaPlayer()
+        player.setDataSource(afd.fileDescriptor, afd.startOffset, afd.length)
+        player.prepare()
+        player.start()
+
+        mediaMetadataRetriever.setDataSource(afd.fileDescriptor, afd.startOffset, afd.length)
+
+        // Music are taken from our assets directory: in this directory, title metadata are well defined for all musics
+        super.title = mediaMetadataRetriever.extractMetadata(MediaMetadataRetriever.METADATA_KEY_TITLE).toString()
+
+        Log.d("TITLE", super.title)
     }
+
 
     override fun guess(titleGuess: String) {
         TODO("Not yet implemented")
     }
-
 }
