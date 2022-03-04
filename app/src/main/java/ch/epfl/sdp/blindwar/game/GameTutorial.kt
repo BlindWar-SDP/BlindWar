e7f51b6887dbdff1bec9574e1f1fdbd99c8c5d17
Marty <mathias.marty@epfl.ch>
2022-03-04 15:12:47 +0100

    Start coding an abstract class for game logic

---
+++ app/src/main/java/ch/epfl/sdp/blindwar/game/GameTutorial.kt
@@ -0,0 +1,12 @@
+package ch.epfl.sdp.blindwar.game
+
+class GameTutorial: Game() {
+    override fun nextRound() {
+        TODO("Not yet implemented")
+    }
+
+    override fun guess(titleGuess: String) {
+        TODO("Not yet implemented")
+    }
+
+}
