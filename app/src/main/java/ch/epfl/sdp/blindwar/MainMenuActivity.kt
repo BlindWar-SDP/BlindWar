ce4328814511077b80c65e9d3585c46a0a5ef486
fwfw fwfwd <henrique.dasilvagameiro@epfl.ch>
2022-03-04 18:32:50 +0100

    main menu design changed and buttons linked to the activities except logout

---
+++ app/src/main/java/ch/epfl/sdp/blindwar/MainMenuActivity.kt
@@ -0,0 +1,32 @@
+package ch.epfl.sdp.blindwar
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class MainMenuActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_menu)
+    }
+
+    // Called when the user taps the Solo button
+    fun soloButton(view: View) {
+        val intent = Intent(this, SoloActivity::class.java)
+        startActivity(intent)
+    }
+
+    // Called when the user taps the Tutorial button
+    fun tutorialButton(view: View) {
+        val intent = Intent(this, TutorialActivity::class.java)
+        startActivity(intent)
+    }
+
+    // Called when the user taps the Solo button
+    fun profileButton(view: View) {
+        val intent = Intent(this, ProfileActivity::class.java)
+        startActivity(intent)
+    }
+
+}
---
+++ app/src/main/java/ch/epfl/sdp/blindwar/SoloActivity.kt
@@ -0,0 +1,11 @@
+package ch.epfl.sdp.blindwar
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SoloActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_solo)
+    }
+}
