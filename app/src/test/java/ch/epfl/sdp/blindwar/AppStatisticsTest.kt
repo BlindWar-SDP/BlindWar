9e842731d1796ddd54a41af5276481067c985dd6
Bud Spencer <karoly.papp@epfl.ch>
2022-03-10 15:24:36 +0100

    added tests for Statistics

--- app/src/test/java/ch/epfl/sdp/blindwar/AppStatisticsTest.kt
+++ app/src/test/java/ch/epfl/sdp/blindwar/AppStatisticsTest.kt
@@ -1,78 +1,135 @@
 package ch.epfl.sdp.blindwar
 
-import org.junit.Assert.*
-
 import org.junit.Test
 
 class AppStatisticsTest {
 
+    private var testStats = AppStatistics() // for individual metrics
+    private var testStats2 = AppStatistics() // for right/win percentage
+    private var testStats3 = AppStatistics() // for wrong/loss percentage
+
     @Test
-    fun getSoloCorrect() {
+    fun getSoloCorrectInitial() {
+        assert(testStats.soloCorrect == 0)
     }
 
     @Test
-    fun getSoloWrong() {
+    fun soloCorrectnessUpdateCorrect() {
+        testStats.soloCorrectnessUpdate(true)
+        assert(testStats.soloCorrect == 1)
+    }
+
+    @Test
+    fun getSoloWrongInitial() {
+        assert(testStats.soloWrong == 0)
+    }
+
+    @Test
+    fun soloCorrectnessUpdateWrong() {
+        testStats.soloCorrectnessUpdate(false)
+        assert(testStats.soloWrong == 1)
     }
 
     @Test
     fun getSoloCorrectPercent() {
+        testStats2.soloCorrectnessUpdate(true)
+        testStats2.soloCorrectnessUpdate(false)
+        assert(testStats2.soloCorrectPercent == 50.0F)
     }
 
     @Test
     fun getSoloWrongPercent() {
+        testStats3.soloCorrectnessUpdate(true)
+        testStats3.soloCorrectnessUpdate(false)
+        assert(testStats3.soloCorrectPercent == 50.0F)
     }
 
     @Test
-    fun getMultiCorrect() {
+    fun getMultiCorrectInitial() {
+        assert(testStats.multiCorrect == 0)
     }
 
     @Test
-    fun getMultiWrong() {
+    fun multiCorrectnessUpdateCorrect() {
+        testStats.multiCorrectnessUpdate(true)
+        assert(testStats.multiCorrect == 1)
+    }
+
+    @Test
+    fun getMultiWrongInitial() {
+        assert(testStats.multiWrong == 0)
+    }
+
+    @Test
+    fun multiCorrectnessUpdateWrong() {
+        testStats.multiCorrectnessUpdate(false)
+        assert(testStats.multiWrong == 1)
     }
 
     @Test
     fun getMultiCorrectPercent() {
+        testStats2.multiCorrectnessUpdate(true)
+        testStats2.multiCorrectnessUpdate(false)
+        assert(testStats2.multiCorrectPercent == 50.0F)
     }
 
     @Test
     fun getMultiWrongPercent() {
+        testStats3.multiCorrectnessUpdate(true)
+        testStats3.multiCorrectnessUpdate(false)
+        assert(testStats3.multiCorrectPercent == 50.0F)
     }
 
     @Test
-    fun getWins() {
+    fun getWinsInitial() {
+        assert(testStats.wins == 0)
     }
 
     @Test
-    fun getLosses() {
+    fun multiWinLossCountUpdateWin() {
+        testStats.multiWinLossCountUpdate(true)
+        assert(testStats.wins == 1)
+    }
+
+    @Test
+    fun getLossesInitial() {
+        assert(testStats.losses == 0)
+    }
+
+    @Test
+    fun multiWinLossCountUpdateLoss() {
+        testStats.multiWinLossCountUpdate(false)
+        assert(testStats.losses == 1)
     }
 
     @Test
     fun getWinPercent() {
+        testStats2.multiWinLossCountUpdate(true)
+        testStats2.multiWinLossCountUpdate(false)
+        assert(testStats2.winPercent == 50.0F)
     }
 
     @Test
     fun getLossPercent() {
+        testStats3.multiWinLossCountUpdate(true)
+        testStats3.multiWinLossCountUpdate(false)
+        assert(testStats3.lossPercent == 50.0F)
     }
 
     @Test
-    fun getElo() {
+    fun getEloInitial() {
+        assert(testStats.elo == 1000)
+    }
+
+    @Test
+    fun getEloAfterStrongWin() {
+        assert(testStats.elo == 1000)
     }
 
     @Test
     fun resetStatistics() {
     }
 
-    @Test
-    fun soloCorrectnessUpdate() {
-    }
-
-    @Test
-    fun multiCorrectnessUpdate() {
-    }
-
-    @Test
-    fun multiWinLossCountUpdate() {
-    }
 
     @Test
     fun eloUpdate() {
