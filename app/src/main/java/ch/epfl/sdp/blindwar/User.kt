4f2eff96a61ee7b88ccc7920a23d0381c09fe800
pkr <pkepfl@outlook.com>
2022-03-18 10:07:28 +0100

    temp commit

--- app/src/main/java/ch/epfl/sdp/blindwar/User.kt
+++ app/src/main/java/ch/epfl/sdp/blindwar/User.kt
@@ -1,12 +1,46 @@
 package ch.epfl.sdp.blindwar
 
 import android.media.Image
+import android.net.Uri
 
 class User(
-    val firstName: String,
-    val lastName: String,
-    val email: String,
-    var screenName: String,
-    var profilePicture: Image?,
-    var userStatistics: AppStatistics
-)
+    var email: String,
+    var userStatistics: AppStatistics,
+
+    var pseudo: String?,
+    var firstName: String?,
+    var lastName: String?,
+    var birthDate: Long?,
+var profilePicture: Uri?,
+) {
+    class Builder(
+        private var email: String,
+        private var userStatistics: AppStatistics,
+
+        private var pseudo: String? = null,
+        private var firstName: String? = null,
+        private var lastName: String? = null,
+        var birthDate: Long? = null,
+        private var profilePicture: Uri? = null,
+
+        ) {
+        fun setLastName(name: String) = apply { this.lastName = name }
+        fun setFirstName(name: String) = apply { this.firstName = name }
+        fun setBirthDate(date: Long) = apply { this.birthDate = date }
+//        fun setImage(link: Uri?) = apply {this.profilePicture = link}
+
+        fun build(): User {
+            return User(
+                email,
+                userStatistics,
+                pseudo,
+                firstName,
+                lastName,
+                birthDate,
+                profilePicture
+            )
+        }
+    }
+
+//    fun registerUser() {}
+}
