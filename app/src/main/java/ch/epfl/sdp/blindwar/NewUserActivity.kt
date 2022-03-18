ffaff394ae72cdbf5d9b15e3fa80f028e399ab6a
pkr <pkepfl@outlook.com>
2022-03-18 10:05:34 +0100

    add NewUserActivity

---
+++ app/src/main/java/ch/epfl/sdp/blindwar/NewUserActivity.kt
@@ -0,0 +1,11 @@
+package ch.epfl.sdp.blindwar
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class NewUserActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_new_user)
+    }
+}
