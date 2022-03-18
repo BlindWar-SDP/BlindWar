9cbc8512e6ded0b8f3478b378c75fda0eaf0f19f
pkr <pkepfl@outlook.com>
2022-03-18 10:25:43 +0100

    temp save

---
+++ app/src/androidTest/java/ch/epfl/sdp/blindwar/NewUserActivityTest.kt
@@ -0,0 +1,37 @@
+package ch.epfl.sdp.blindwar
+
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions
+import androidx.test.espresso.intent.Intents
+import androidx.test.espresso.intent.matcher.IntentMatchers
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import junit.framework.TestCase
+import org.junit.After
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+
+class NewUserActivityTest : TestCase() {
+
+    @get:Rule
+    var testRule = ActivityScenarioRule(
+        NewUserActivity::class.java
+    )
+    @Before
+    fun setup() {
+        Intents.init()
+    }
+
+    @After
+    fun cleanup() {
+        Intents.release()
+    }
+
+    @Test
+    fun testConfirmButton() {
+        onView(withId(R.id.NU_Validate_Btn))
+            .perform(ViewActions.click())
+        Intents.intended(IntentMatchers.hasComponent(MainMenuActivity::class.java.name))
+    }
+}
