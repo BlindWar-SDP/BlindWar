75160f21e31877d970f505244004f31722d828e6
ghemDD <nael.ouerghemi14@gmail.com>
2022-03-18 11:13:58 +0100

    fix test coverage (androidTest only)

--- app/src/test/java/ch/epfl/sdp/blindwar/database/UserDatabaseTest.kt
+++ app/src/test/java/ch/epfl/sdp/blindwar/database/UserDatabaseTest.kt
@@ -1,15 +1,10 @@
 package ch.epfl.sdp.blindwar.database
 
-import android.media.Image
-import ch.epfl.sdp.blindwar.AppStatistics
-import ch.epfl.sdp.blindwar.User
 import junit.framework.TestCase
 import org.junit.Assert
 import org.junit.Test
 
 class UserDatabaseTest : TestCase() {
-
-
     @Test
     fun testAddUser() {
         Assert.assertEquals(4, 2 + 2)
