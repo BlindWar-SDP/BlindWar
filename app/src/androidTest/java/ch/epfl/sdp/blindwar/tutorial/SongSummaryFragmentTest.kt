a325461b62cf2ea70d8c9838f36ad249df3b0574
ghemDD <nael.ouerghemi14@gmail.com>
2022-03-21 22:53:37 +0100

    test like anim

--- app/src/androidTest/java/ch/epfl/sdp/blindwar/tutorial/SongSummaryFragmentTest.kt
+++ app/src/androidTest/java/ch/epfl/sdp/blindwar/tutorial/SongSummaryFragmentTest.kt
@@ -3,9 +3,9 @@ package ch.epfl.sdp.blindwar.tutorial
 import android.os.Bundle
 import androidx.fragment.app.testing.launchFragmentInContainer
 import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions.matches
-import androidx.test.espresso.matcher.ViewMatchers.withId
-import androidx.test.espresso.matcher.ViewMatchers.withText
+import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import ch.epfl.sdp.blindwar.R
 import ch.epfl.sdp.blindwar.domain.game.SongImageUrlConstants.SONG_MAP
@@ -28,5 +28,18 @@ class SongSummaryFragmentTest {
         launchFragmentInContainer<SongSummaryFragment>(bundle)
         onView((withId(R.id.artistTextView))).check(matches(withText("Daft Punk")))
         onView((withId(R.id.trackTextView))).check(matches(withText("One More Time")))
+    }
+
+    @Test
+    fun testLikeAnimation() {
+        val bundle = Bundle().let {
+            it.putString("Artist", "Daft Punk")
+            it.putString("Title", "One More Time")
+            it.putString("Image", SONG_MAP["Daft Punk"])
+            it
+        }
+
+        val scenario = launchFragmentInContainer<SongSummaryFragment>(bundle)
+        onView(withId(R.id.likeView)).perform(click()).check(matches(isDisplayed()))
     }
 }
--- app/src/main/java/ch/epfl/sdp/blindwar/ui/tutorial/SongSummaryFragment.kt
+++ app/src/main/java/ch/epfl/sdp/blindwar/ui/tutorial/SongSummaryFragment.kt
@@ -12,8 +12,13 @@ import ch.epfl.sdp.blindwar.R
 import com.airbnb.lottie.LottieAnimationView
 import com.airbnb.lottie.LottieDrawable
 import com.squareup.picasso.Picasso
+import kotlin.properties.Delegates
 
 class SongSummaryFragment: Fragment() {
+    private lateinit var likeAnim: LottieAnimationView
+    private lateinit var confAnim: LottieAnimationView
+    private var likeSwitch: Boolean = false
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -26,13 +31,23 @@ class SongSummaryFragment: Fragment() {
         val artistText = view.findViewById<TextView>(R.id.artistTextView)
         val trackText = view.findViewById<TextView>(R.id.trackTextView)
 
-        val likeAnim = view.findViewById<LottieAnimationView>(R.id.animationView)
-        val confAnim = view.findViewById<LottieAnimationView>(R.id.particleView)
-        var likeSwitch = false
+        likeAnim = view.findViewById(R.id.likeView)
+        setLikeListener()
+
+        confAnim = view.findViewById(R.id.particleView)
+        likeSwitch = false
 
         //confAnim.playAnimation()
         //confAnim.repeatCount = 0
 
+        artistText.text = arguments?.get("Artist").toString()
+        trackText.text = arguments?.get("Title").toString()
+        Picasso.get().load(arguments?.get("Image")!!.toString()).into(artistView)
+
+        return view
+    }
+
+    private fun setLikeListener() {
         likeAnim.setOnClickListener{
             if (!likeSwitch) {
                 likeAnim.setMinAndMaxFrame(10, 30)
@@ -49,12 +64,5 @@ class SongSummaryFragment: Fragment() {
 
             likeSwitch = !likeSwitch
         }
-
-        artistText.text = arguments?.get("Artist").toString()
-        trackText.text = arguments?.get("Title").toString()
-
-        Picasso.get().load(arguments?.get("Image")!!.toString()).into(artistView)
-
-        return view
     }
 }
