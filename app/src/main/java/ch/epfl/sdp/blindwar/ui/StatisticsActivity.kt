e15a0b93bfd6a8106e7f8462c4ccebfc204156bb
Bud Spencer <karoly.papp@epfl.ch>
2022-03-24 21:18:58 +0100

    statistics page v2

--- app/src/main/java/ch/epfl/sdp/blindwar/ui/StatisticsActivity.kt
+++ app/src/main/java/ch/epfl/sdp/blindwar/ui/StatisticsActivity.kt
@@ -3,18 +3,53 @@ package ch.epfl.sdp.blindwar.ui
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+import android.widget.Toast
+
 import androidx.appcompat.app.AppCompatActivity
 import ch.epfl.sdp.blindwar.R
 
+
 class StatisticsActivity : AppCompatActivity() {
+
+    //Float values for stats (only for demo)
+    private var wins = 45
+    private var losses = 35
+    private var draws = 100 - wins - losses
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_statistics)
+        // access the items of the list
+        val modes = resources.getStringArray(R.array.modes_array)
+
+        // access the spinner
+        val spinner = findViewById<Spinner>(R.id.modes_spinner)
+        if (spinner != null) {
+            val adapter = ArrayAdapter(this,
+                R.layout.spinner_item, modes)
+            spinner.adapter = adapter
+
+            spinner.onItemSelectedListener = object :
+                AdapterView.OnItemSelectedListener {
+                override fun onItemSelected(parent: AdapterView<*>,
+                                            view: View, position: Int, id: Long) {
+                    Toast.makeText(this@StatisticsActivity,
+                        getString(R.string.selected_item) + " " +
+                                "" + modes[position], Toast.LENGTH_SHORT).show()
+                }
+
+                override fun onNothingSelected(parent: AdapterView<*>) {
+                    // write code to perform some action
+                }
+            }
+        }
     }
 
 
-    fun backToProfileButton(view: View) {
-        startActivity(Intent(this, ProfileActivity::class.java))
-    }
+
+
 }
