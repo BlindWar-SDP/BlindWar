7c9bd98b64eddb223ceac063b450be219ea35853
Monnet Joris <joris.monnet@epfl.ch>
2022-03-24 02:19:59 +0100

    remove tests

--- app/src/test/java/ch/epfl/sdp/blindwar/domain/game/VoiceRecognizerTest.kt
+++ app/src/test/java/ch/epfl/sdp/blindwar/domain/game/VoiceRecognizerTest.kt
@@ -1,14 +1,6 @@
 package ch.epfl.sdp.blindwar.domain.game
 
 import junit.framework.TestCase
-import org.junit.Test
 
 class VoiceRecognizerTest : TestCase() {
-
-    private val voiceRecognizer: VoiceRecognizer = VoiceRecognizer()
-
-    @Test
-    fun start() {
-        assert(voiceRecognizer.resultsRecognized == "")
-    }
 }
