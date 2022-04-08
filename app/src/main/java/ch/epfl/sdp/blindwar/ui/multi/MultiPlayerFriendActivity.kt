188931b9ebb5d7c58f48acb21b2ddca42e1fa569
bastiano.samsung <bastiano.samsung@gmail.com>
2022-04-08 07:52:55 +0200

    multiplayer actionbar back

--- app/src/main/java/ch/epfl/sdp/blindwar/ui/multi/MultiPlayerFriendActivity.kt
+++ app/src/main/java/ch/epfl/sdp/blindwar/ui/multi/MultiPlayerFriendActivity.kt
@@ -1,6 +1,7 @@
 package ch.epfl.sdp.blindwar.ui.multi
 
 import android.os.Bundle
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import ch.epfl.sdp.blindwar.R
 
@@ -14,6 +15,19 @@ class MultiPlayerFriendActivity : AppCompatActivity(){
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_multifriend)
+
+        // showing the back button in action bar
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            android.R.id.home -> {
+                onBackPressed()
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 
 }
