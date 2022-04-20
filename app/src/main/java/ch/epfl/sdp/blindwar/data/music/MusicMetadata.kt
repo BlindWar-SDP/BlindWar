fe29ea6412a433baaa09993ae00714499466c3cc
marluxiaboss <henrique.dasilvagameiro@epfl.ch>
2022-04-20 18:29:12 +0200

    no-argument constructor added to musicMetadata class by adding default values

--- app/src/main/java/ch/epfl/sdp/blindwar/data/music/MusicMetadata.kt
+++ app/src/main/java/ch/epfl/sdp/blindwar/data/music/MusicMetadata.kt
@@ -2,10 +2,10 @@ package ch.epfl.sdp.blindwar.data.music
 
 import android.net.Uri
 
-abstract class MusicMetadata(val title: String,
-                         val artist: String,
+abstract class MusicMetadata(val title: String = "title",
+                         val artist: String = "artist",
                          val imageUrl: String? = null,
-                         val duration: Int) {
+                         val duration: Int = 0) {
     override fun toString(): String = "$title by $artist"
 }
 
