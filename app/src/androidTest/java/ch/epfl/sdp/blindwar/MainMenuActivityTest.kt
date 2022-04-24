22ba755229e00afe30cc84678f6e4cba862bc458
ghemDD <nael.ouerghemi14@gmail.com>
2022-04-24 15:46:19 +0200

    adapt login layout to smaller screen

--- app/src/androidTest/java/ch/epfl/sdp/blindwar/MainMenuActivityTest.kt
+++ app/src/androidTest/java/ch/epfl/sdp/blindwar/MainMenuActivityTest.kt
@@ -4,15 +4,10 @@ import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.intent.Intents
-import androidx.test.espresso.intent.Intents.intended
-import androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent
 import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
-import androidx.test.rule.GrantPermissionRule
-import ch.epfl.sdp.blindwar.game.multi.MultiPlayerActivity
-import ch.epfl.sdp.blindwar.game.solo.SoloActivity
 import ch.epfl.sdp.blindwar.menu.MainMenuActivity
 import junit.framework.TestCase
 import org.junit.After
