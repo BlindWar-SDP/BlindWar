ac24c87973c31eb1e19a610419e125b388823108
marluxiaboss <henrique.dasilvagameiro@epfl.ch>
2022-04-28 09:04:23 +0200

    create new xml for the history activity

--- app/src/main/java/ch/epfl/sdp/blindwar/profile/HistoryActivity.kt
+++ app/src/main/java/ch/epfl/sdp/blindwar/profile/HistoryActivity.kt
@@ -20,14 +20,14 @@ class HistoryActivity : AppCompatActivity() {
      */
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main_menu)
+        setContentView(R.layout.activity_history_menu)
 
         showFragment(PlayMenuFragment())
 
         val likedMusicType = "liked musics"
         val matchHistoryType = "match history"
 
-        findViewById<BottomNavigationView>(R.id.bottomNavigationView).setOnItemSelectedListener {
+        findViewById<BottomNavigationView>(R.id.historyBottomNavigationView).setOnItemSelectedListener {
             when(it.itemId){
                 R.id.item_liked_musics-> showFragment(DisplayHistoryFragment.newInstance(likedMusicType))
                 R.id.item_match_history -> showFragment(DisplayHistoryFragment.newInstance(likedMusicType))
@@ -43,7 +43,7 @@ class HistoryActivity : AppCompatActivity() {
      */
     private fun showFragment(fragment: Fragment) {
         supportFragmentManager.beginTransaction().apply {
-            replace(R.id.fragment_menu_container, fragment)
+            replace(R.id.fragment_history_menu_container, fragment)
             commit()
         }
     }
