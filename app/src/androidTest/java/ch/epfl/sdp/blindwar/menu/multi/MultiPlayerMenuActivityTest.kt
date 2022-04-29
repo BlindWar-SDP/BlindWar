4e1a94addb2606aaf3d5375577a8ab26f8e6f3cc
Marty <mathias.marty@epfl.ch>
2022-04-29 04:15:29 +0200

    Add the menu activity to the android manifest

--- app/src/androidTest/java/ch/epfl/sdp/blindwar/menu/multi/MultiPlayerMenuActivityTest.kt
+++ app/src/androidTest/java/ch/epfl/sdp/blindwar/menu/multi/MultiPlayerMenuActivityTest.kt
@@ -7,7 +7,6 @@ import androidx.test.espresso.intent.matcher.IntentMatchers
 import androidx.test.espresso.matcher.ViewMatchers
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import ch.epfl.sdp.blindwar.R
-import ch.epfl.sdp.blindwar.game.multi.MultiPlayerActivity
 import ch.epfl.sdp.blindwar.game.multi.MultiPlayerFriendActivity
 import ch.epfl.sdp.blindwar.game.multi.MultiPlayerRandomActivity
 import ch.epfl.sdp.blindwar.ui.multi.MultiPlayerMenuActivity
