e9170915d09ea48bb8c25cb6c5d554ee499419a9
Monnet Joris <joris.monnet@epfl.ch>
2022-05-13 04:00:53 +0200

    add tests

--- app/src/androidTest/java/ch/epfl/sdp/blindwar/database/MatchDatabaseTest.kt
+++ app/src/androidTest/java/ch/epfl/sdp/blindwar/database/MatchDatabaseTest.kt
@@ -2,7 +2,11 @@ package ch.epfl.sdp.blindwar.database
 
 import androidx.fragment.app.testing.launchFragmentInContainer
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import ch.epfl.sdp.blindwar.game.model.config.GameInstance
 import ch.epfl.sdp.blindwar.profile.fragments.ProfileFragment
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 import junit.framework.TestCase
 import org.junit.Before
 import org.junit.Test
@@ -21,7 +25,60 @@ class MatchDatabaseTest : TestCase() {
     @Test
     fun testMatchCreation() {
         launchFragmentInContainer<ProfileFragment>()
-        //
-        //MatchDatabase.createMatch(User(), 1, GameUtil.gameInstance, db)
+        val match = MatchDatabase.createMatch(
+            "test",
+            "test",
+            1000,
+            2,
+            GameInstance.Builder().build(),
+            Firebase.firestore,
+            false
+        )
+        assertTrue(match?.listPlayers!!.contains("test"))
+    }
+
+    @Test
+    fun testMatchCreationNull() {
+        launchFragmentInContainer<ProfileFragment>()
+        val match = MatchDatabase.createMatch(
+            "",
+            "test",
+            1000,
+            2,
+            GameInstance.Builder().build(),
+            Firebase.firestore,
+            false
+        )
+        assertTrue(match == null)
+    }
+
+    @Test
+    fun testMatchRemove() {
+        launchFragmentInContainer<ProfileFragment>()
+        val match = MatchDatabase.createMatch(
+            "test",
+            "test",
+            1000,
+            2,
+            GameInstance.Builder().build(),
+            Firebase.firestore,
+            false
+        )
+        MatchDatabase.removeMatch("test", FirebaseFirestore.getInstance())
+    }
+
+    @Test
+    fun testGetMatch() {
+        launchFragmentInContainer<ProfileFragment>()
+        val match = MatchDatabase.createMatch(
+            "test",
+            "test",
+            1000,
+            2,
+            GameInstance.Builder().build(),
+            Firebase.firestore,
+            false
+        )
+        assertTrue(MatchDatabase.getMatchSnapshot("test", Firebase.firestore) != null)
     }
 }
