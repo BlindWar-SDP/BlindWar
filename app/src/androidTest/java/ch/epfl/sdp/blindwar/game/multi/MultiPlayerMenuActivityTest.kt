d484e0edb0e2614d91ee41e678af0dd3f1e58b08
Monnet Joris <joris.monnet@epfl.ch>
2022-05-13 10:29:03 +0200

    add squeleton of tests

--- app/src/androidTest/java/ch/epfl/sdp/blindwar/game/multi/MultiPlayerMenuActivityTest.kt
+++ app/src/androidTest/java/ch/epfl/sdp/blindwar/game/multi/MultiPlayerMenuActivityTest.kt
@@ -1,24 +1,16 @@
 package ch.epfl.sdp.blindwar.game.multi
 
-import androidx.test.espresso.Espresso
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions
-import androidx.test.espresso.action.ViewActions.pressBack
 import androidx.test.espresso.action.ViewActions.scrollTo
-import androidx.test.espresso.assertion.ViewAssertions
-import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.intent.Intents
 import androidx.test.espresso.intent.Intents.intended
-import androidx.test.espresso.intent.matcher.IntentMatchers
 import androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent
-import androidx.test.espresso.matcher.ViewMatchers
-import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import ch.epfl.sdp.blindwar.R
-import ch.epfl.sdp.blindwar.game.util.GameActivity
 import ch.epfl.sdp.blindwar.menu.MainMenuActivity
 import org.junit.After
-import org.junit.Assert.assertEquals
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
@@ -45,23 +37,17 @@ class MultiPlayerMenuActivityTest {
         intended(hasComponent(MainMenuActivity::class.java.name))
     }
 
-    /*@Test
+    @Test
     fun testDisplayFriendButtonAndClose() {
         onView(withId(R.id.imageFriendsButton)).perform(scrollTo(), ViewActions.click())
-        Thread.sleep(1000)
+        //TODO
+    }
 
-        // Click on close
-        onView(isRoot()).perform(ViewActions.pressBack());
-        Thread.sleep(1000)
-
-        onView(withId(R.id.imageFriendsButton)).check(matches(isDisplayed()))
-    }*/
-
-    /*@Test
+    @Test
     fun testDisplayRandomButton() {
         onView(withId(R.id.imageRandomButton)).perform(scrollTo(), ViewActions.click())
-        onView(withId(R.id.imageRandomButton)).check(matches(isDisplayed()))
-    }*/
+        //TODO
+    }
 
     @Test
     fun testCreateButton() {
