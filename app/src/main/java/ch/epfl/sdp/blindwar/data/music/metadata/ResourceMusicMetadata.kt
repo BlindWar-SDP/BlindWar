1097131759b04d17b2af2518491327fe66d34dd9
Monnet Joris <joris.monnet@epfl.ch>
2022-05-26 20:28:39 +0200

    fix tests

--- app/src/main/java/ch/epfl/sdp/blindwar/data/music/metadata/ResourceMusicMetadata.kt
+++ app/src/main/java/ch/epfl/sdp/blindwar/data/music/metadata/ResourceMusicMetadata.kt
@@ -1,12 +1,13 @@
-/*
 package ch.epfl.sdp.blindwar.data.music.metadata
 
 class ResourceMusicMetadata(
-    val resourceId: Int
-) : MusicMetadata() {
+    title: String,
+    artist: String,
+    imageUrl: String,
+    duration: Int,
+) : MusicMetadata(title, artist, imageUrl, duration) {
     override fun getPreviewUrl(): String {
         return ""
     }
 }
 
- */
--- app/src/test/java/ch/epfl/sdp/blindwar/data/music/metadata/ResourceMusicMetadataTest.kt
+++ app/src/test/java/ch/epfl/sdp/blindwar/data/music/metadata/ResourceMusicMetadataTest.kt
@@ -8,20 +8,20 @@ import org.hamcrest.MatcherAssert
 class ResourceMusicMetadataTest : TestCase() {
     fun testTestToString() {
         val expected = "Poker Face by Lady Gaga"
-        val uriMetadata = ResourceMusicMetadata("Poker Face", "Lady Gaga", "", 0, 90)
+        val uriMetadata = ResourceMusicMetadata("Poker Face", "Lady Gaga", "", 0)
         MatcherAssert.assertThat(uriMetadata.toString(), CoreMatchers.`is`(expected))
     }
 
     fun testGetTitle() {
         val expected = "Lady Gaga"
-        val uriMetadata = ResourceMusicMetadata("Poker Face", "", "", 0, 90)
+        val uriMetadata = ResourceMusicMetadata("Poker Face", "", "", 0)
         uriMetadata.artist = expected
         MatcherAssert.assertThat(uriMetadata.artist, CoreMatchers.`is`(expected))
     }
 
     fun testGetArtist() {
         val expected = "Shine On You Crazy Diamond"
-        val uriMetadata = ResourceMusicMetadata("", "Pink Floyd", "", 650000, 90)
+        val uriMetadata = ResourceMusicMetadata("", "Pink Floyd", "", 650000)
         uriMetadata.title = expected
         MatcherAssert.assertThat(uriMetadata.title, CoreMatchers.`is`(expected))
     }
@@ -29,7 +29,7 @@ class ResourceMusicMetadataTest : TestCase() {
     fun testGetImageUrl() {
         val expected = "https://i.scdn.co/image/ab67616d0000b273b33d46dfa2635a47eebf63b2"
         val uriMetadata =
-            ResourceMusicMetadata("Shine On You Crazy Diamond", "Pink Floyd", "", 650000, 90)
+            ResourceMusicMetadata("Shine On You Crazy Diamond", "Pink Floyd", "", 650000)
         uriMetadata.imageUrl = expected
         MatcherAssert.assertThat(uriMetadata.imageUrl, CoreMatchers.`is`(expected))
     }
@@ -37,15 +37,8 @@ class ResourceMusicMetadataTest : TestCase() {
     fun testGetDuration() {
         val expected = 90
         val uriMetadata =
-            ResourceMusicMetadata("Shine On You Crazy Diamond", "Pink Floyd", "", 650000, 90)
+            ResourceMusicMetadata("Shine On You Crazy Diamond", "Pink Floyd", "", 650000)
         uriMetadata.duration = expected
         MatcherAssert.assertThat(uriMetadata.duration, CoreMatchers.`is`(expected))
-    }
-
-    fun testGetResourceID() {
-        val expected = 90
-        val uriMetadata =
-            ResourceMusicMetadata("Shine On You Crazy Diamond", "Pink Floyd", "", 650000, expected)
-        MatcherAssert.assertThat(uriMetadata.resourceId, CoreMatchers.`is`(expected))
     }
 }
