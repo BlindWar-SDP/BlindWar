b6222ef97a4eacc47dac73f1a8fe46ad4cad0ff1
Marty <mathias.marty@epfl.ch>
2022-05-27 10:25:02 +0200

    Add test

--- app/src/main/java/ch/epfl/sdp/blindwar/game/solo/fragments/DemoFragment.kt
+++ app/src/main/java/ch/epfl/sdp/blindwar/game/solo/fragments/DemoFragment.kt
@@ -121,7 +121,7 @@ class DemoFragment : Fragment() {
 
         // if multi mode, get gameInstance from matchId
         matchId = arguments?.getString("match_id")
-        
+
         // Get the scoreboard
         scoreboard = view.findViewById(R.id.scoreboard)
 
